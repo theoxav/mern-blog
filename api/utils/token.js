94164c433c56eed0d4f437693c6edbb02b03f196
theoxav <theoxxav@outlook.fr>
2024-10-14 19:42:01 +0200

    Add 'isAdmin' to the User model

--- api/utils/token.js
+++ api/utils/token.js
@@ -2,7 +2,10 @@ import jwt from "jsonwebtoken";
 import config from "../config/config.js";
 
 export const generateToken = (user, res) => {
-  const token = jwt.sign({ id: user._id }, config.JWT_SECRET);
+  const token = jwt.sign(
+    { id: user._id, isAdmin: user.isAdmin },
+    config.JWT_SECRET
+  );
   const { password, ...rest } = user._doc;
   res
     .status(200)
