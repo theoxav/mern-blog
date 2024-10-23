0650ce2d174be1e2fa5b8c4448612147c5f0feb0
theoxav <theoxxav@outlook.fr>
2024-10-23 16:09:09 +0200

    Complete projects and about pages

--- client/src/pages/ProjectsPage.jsx
+++ client/src/pages/ProjectsPage.jsx
@@ -1,3 +1,14 @@
+import CallToAction from "../components/US/CallToAction";
+
 export default function ProjectsPage() {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex flex-col justify-center items-center gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Build fun and engaging projects while learning HTML, CSS, and
+        Javascript!
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
